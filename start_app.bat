65b245764cb5e34888b7a614f07ea862295f67ca
SleepyCorgi <Bobbyquach90@gmail.com>
2026-04-09 16:56:14 -0400

    adjust batch

--- start_app.bat
+++ start_app.bat
@@ -1,11 +1,14 @@
 @echo off
 setlocal
-cd /d "%~dp0"
+cd /d %~dp0
 
-if exist "venv\Scripts\activate.bat" (
-  start "Steam NLP Scraper" cmd /k "call venv\Scripts\activate.bat && python run.py"
+:: Launch the browser in a new window/tab immediately
+start "" "http://127.0.0.1:8000/"
+
+if exist venv\Scripts\activate.bat (
+    start "Steam NLP Scraper" cmd /k "call venv\Scripts\activate.bat && python run.py"
 ) else (
-  start "Steam NLP Scraper" cmd /k "python run.py"
+    start "Steam NLP Scraper" cmd /k "python run.py"
 )
 
 endlocal
